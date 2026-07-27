5ebd8fdb8c50cae72a9b04888e60e7016c3cb910
Nikolai <mail@nlavrov.ru>
2026-07-27 12:29:39 +0700

    v0.1

--- const.py
+++ const.py
@@ -1,29 +1,26 @@
-"""Constants for RTKey."""
-
-from datetime import timedelta
+"""
+Constants for RTKey.
+"""
 
 from datetime import timedelta
 
 DOMAIN = "rtkey"
 
-SCAN_INTERVAL = timedelta(seconds=30)
+DEFAULT_NAME = "Ростелеком Ключ"
 
 MANUFACTURER = "Ростелеком"
 
 MODEL = "RT Key"
 
-NAME = "Ростелеком Ключ"
+SCAN_INTERVAL = timedelta(seconds=30)
 
 CONF_PHONE = "phone"
-
-CONF_TOKEN = "access_token"
-
+CONF_CODE = "code"
+CONF_CODE_ID = "code_id"
+CONF_ACCESS_TOKEN = "access_token"
 CONF_DEVICE_ID = "device_id"
 
 API_URL = "https://household.key.rt.ru"
-
 AUTH_URL = "https://keyapis.key.rt.ru"
-
 CAMERA_URL = "https://media-vdk4.camera.rt.ru"
-
 VC_URL = "https://household.key.rt.ru/vc/api/v1"
